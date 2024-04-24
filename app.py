d95b0d692124d65d30da392658648e4d9ec5dc47
aditya17z <137027615+aditya17z@users.noreply.github.com>
2024-04-24 21:48:00 +0530

    app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import pandas as pd
 import time 
 from datetime import datetime
 
-ts=time.time()
-date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
-timestamp=datetime.fromtimestamp(ts).strftime("%H:%M-%S")
+ts = time.time()
+date = datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+timestamp = datetime.fromtimestamp(ts).strftime("%H:%M-%S")
 
 from streamlit_autorefresh import st_autorefresh
+
+st.title("Facial Attendance")
 
 count = st_autorefresh(interval=2000, limit=100, key="fizzbuzzcounter")
 
@@ -22,7 +24,6 @@ elif count % 5 == 0:
 else:
     st.write(f"Count: {count}")
 
-
-df=pd.read_csv("Attendance/Attendance_" + date + ".csv")
+df = pd.read_csv("Attendance/Attendance_" + date + ".csv")
 
 st.dataframe(df.style.highlight_max(axis=0))
